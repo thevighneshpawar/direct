c551647a810d7f93e2cf6e9bdcba5fc03a4cf015
Vighnesh Pawar <vighneshpawar004@gmail.com>
2024-03-10 10:01:27 +0530

    final

--- login.js
+++ login.js
@@ -1,9 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
   
-    const navLinks = document.getElementById('company-links');
-    const togglebtn = document.getElementById('nav-toggle');
-    console.log('Button clicked!');
-    togglebtn.addEventListener('click', ()=>{
-      navLinks.classList.toggle('active');
-    });
-  })
+//     const navLinks = document.getElementById('company-links');
+//     const togglebtn = document.getElementById('nav-toggle');
+//     console.log('Button clicked!');
+//     togglebtn.addEventListener('click', ()=>{
+//       navLinks.classList.toggle('active');
+//     });
+//   })
+
+  function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var toggleIcon = document.querySelector('.password-toggle');
+    
+    // Check if the password is currently visible
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      toggleIcon.classList.add('fa-eye-slash');
+      toggleIcon.classList.remove('fa-eye');
+    } else {
+      passwordInput.type = 'password';
+      toggleIcon.classList.add('fa-eye');
+      toggleIcon.classList.remove('fa-eye-slash');
+    }
+  }
+  
