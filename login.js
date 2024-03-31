26229e1b5e3b2cdb910f04633b3ead03771fe68a
thevighneshpawar <112792276+thevighneshpawar@users.noreply.github.com>
2024-03-31 11:19:02 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
-// document.addEventListener('DOMContentLoaded', () => {
+ document.addEventListener('DOMContentLoaded', () => {
   
-//     const navLinks = document.getElementById('company-links');
-//     const togglebtn = document.getElementById('nav-toggle');
-//     console.log('Button clicked!');
-//     togglebtn.addEventListener('click', ()=>{
-//       navLinks.classList.toggle('active');
-//     });
-//   })
+    const navLinks = document.getElementById('company-links');
+   const togglebtn = document.getElementById('nav-toggle');
+    console.log('Button clicked!');
+   togglebtn.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active');
+  });
+ })
 
   function togglePasswordVisibility() {
     var passwordInput = document.getElementById('password');
